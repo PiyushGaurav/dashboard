b908e44c5944366915b76103732b6b47fed2aa05
PiyushGaurav <piyushgaurav07@gmail.com>
2024-04-01 17:21:55 +0530

    installed navigation

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function FeedScreen() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Feed Screen</Text>
+		</View>
+	);
+}
+
+function ArticleScreen() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Article Screen</Text>
+		</View>
+	);
+}
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+	return (
+		<Drawer.Navigator>
+			<Drawer.Screen name="Home" component={HomeScreen} />
+			<Drawer.Screen name="Feed" component={FeedScreen} />
+			<Drawer.Screen name="Article" component={ArticleScreen} />
+		</Drawer.Navigator>
+	);
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen options={{ headerShown: false }} name="Home" component={MyDrawer} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+
+export default App;
